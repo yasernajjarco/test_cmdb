929554e720746ee81c38767f4133b7fcace8b853
yasernajjarco <yaser.najjarco@hotmail.com>
2021-02-15 21:44:49 +0100

    remove some files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
-import models from './models';
 import routes from './routes';
 
 const app = express();
@@ -12,17 +11,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   req.context = {
     models,
     me: models.users[1],
   };
   next();
-});
+}); */
 
-app.use('/session', routes.session);
 app.use('/users', routes.user);
-app.use('/messages', routes.message);
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
