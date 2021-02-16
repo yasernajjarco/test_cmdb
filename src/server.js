a85daa009842894183b0782705df89e10a29858c
yasernajjarco <yaser.najjarco@hotmail.com>
2021-02-16 13:52:31 +0100

    welcome

--- src/server.js
+++ src/server.js
@@ -13,13 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/* app.use((req, res, next) => {
-  req.context = {
-    models,
-    me: models.users[1],
-  };
-  next();
-}); */
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to CMDB API service." });
+});
 
 initial.seed();
 
