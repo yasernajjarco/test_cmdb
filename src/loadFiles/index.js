6c7f5220d726b418aecdc5bc2731d4123e393f7f
yasernajjarco <yaser.najjarco@hotmail.com>
2021-02-24 09:56:56 +0100

    hardware insert

--- src/loadFiles/index.js
+++ src/loadFiles/index.js
@@ -5,10 +5,7 @@ const insertProviders = require('./insertProviders');
 const insertSofts = require('./insertSofts');
 const db = require("../index.db");
 
-
-
-
-
+const insertHardwares = require('./insertHardwares');
 
 exports.test = () => {
 
@@ -25,26 +22,37 @@ exports.test = () => {
         files.forEach(function (file) {
             
             switch (file) {
-                 case 'Refs B.xlsx' : 
-                 file = path.resolve( __dirname, "Documents/" + file )
-                 insertProviders.insertProvidersB(file,file.substring(file.lastIndexOf(" ")).trim().charAt(0));
 
-                  break;
+                
+                    case 'Refs B.xlsx' : 
+                    file = path.resolve( __dirname, "Documents/" + file )
+                    insertProviders.insertProvidersB(file,file.substring(file.lastIndexOf(" ")).trim().charAt(0));
+                    break;
 
-                  case 'Refs Z.xlsx' : 
-                  file = path.resolve( __dirname, "Documents/" + file )
-                  insertProviders.insertProvidersB(file,file.substring(file.lastIndexOf(" ")).trim().charAt(0));
-                   break; 
-
-                   case 'CI software B.xlsx' : 
-                  file = path.resolve( __dirname, "Documents/" + file )
-                  insertSofts.insertApplication(file,file.substring(file.lastIndexOf(" ")).trim().charAt(0));
-                   break;
-
-                    case 'CI software Z.xlsx' : 
-                   file = path.resolve( __dirname, "Documents/" + file )
-                   insertSofts.insertApplication(file,file.substring(file.lastIndexOf(" ")).trim().charAt(0));
+                    case 'Refs Z.xlsx' : 
+                    file = path.resolve( __dirname, "Documents/" + file )
+                    insertProviders.insertProvidersB(file,file.substring(file.lastIndexOf(" ")).trim().charAt(0));
                     break; 
+
+                    case 'CI software B.xlsx' : 
+                    file = path.resolve( __dirname, "Documents/" + file )
+                    insertSofts.insertApplication(file,file.substring(file.lastIndexOf(" ")).trim().charAt(0));
+                    break;
+
+                        case 'CI software Z.xlsx' : 
+                    file = path.resolve( __dirname, "Documents/" + file )
+                    insertSofts.insertApplication(file,file.substring(file.lastIndexOf(" ")).trim().charAt(0));
+                        break; 
+
+                        case 'CI hardware B.xlsx' : 
+                        file = path.resolve( __dirname, "Documents/" + file )
+                        insertHardwares.insert(file,file.substring(file.lastIndexOf(" ")).trim().charAt(0));
+                        break; 
+
+                        case 'CI hardware Z.xlsx' : 
+                        file = path.resolve( __dirname, "Documents/" + file )
+                        insertHardwares.insert(file,file.substring(file.lastIndexOf(" ")).trim().charAt(0));
+                        break;
               } 
         });
     });
