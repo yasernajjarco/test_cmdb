dbe96cb49e621ce1c473660e47aaf33c032be446
yasernajjarco <yaser.najjarco@hotmail.com>
2021-03-05 10:37:40 +0100

    config

--- src/config/config.js
+++ src/config/config.js
@@ -1,31 +1,43 @@
 module.exports = {
- 
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
 
-  /** DATABASE */
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+
+    /** DATABASE */
+    /* 
+        HOST: "localhost",
+        USER: "root",
+        PASSWORD: "rootroot",
+        DB: "cmdb",
+        dialect: "mariadb",
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000
+        }, */
+
 
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "rootroot",
-    DB: "cmdb",
+    USER: "lsyalh",
+    PASSWORD: "lsyalh",
+    DB: "dbCMDBdev",
     dialect: "mariadb",
     pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     },
-  
-  
 
-  /** AUTH KEY */
-  auth: {
-    secret: "jllgshllWEUJHGHYJkjsfjds90",
-    expiresIn: 86400
-  }
+
+
+    /** AUTH KEY */
+    auth: {
+        secret: "jllgshllWEUJHGHYJkjsfjds90",
+        expiresIn: 86400
+    }
 };
 
 
-//ok 
-
+//ok
