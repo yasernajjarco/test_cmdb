b2c1f4091e34308c6aaa4f52d80edde97cabec6b
yasernajjarco <yaser.najjarco@hotmail.com>
2021-03-07 21:47:24 +0100

    t

--- src/controllers/ci.controller.js
+++ src/controllers/ci.controller.js
@@ -7,31 +7,30 @@ const classService = db.classService;
 const { Sequelize, DataTypes, Op } = require("sequelize");
 
 exports.findAll = (req, res) => {
- // const title = req.query.title;
- // var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-//{ include: ["ci"] }
-ci.findAll({ include: ["platforms","status","classService"] })
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving Platforms."
-      });
-    });
+    // const title = req.query.title;
+    // var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+    //{ include: ["ci"] }
+    ci.findAll({ include: ["platforms", "status", "classService"] })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving Platforms."
+            });
+        });
 };
 
 
 exports.findAllApp = (req, res) => {
-  // const title = req.query.title;
-  // var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
- // { include: ["ci"] }
- // { include: [ { all: true, nested: true }] }
- //        attributes: ['ManagerName']
+    // const title = req.query.title;
+    // var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+    // { include: ["ci"] }
+    // { include: [ { all: true, nested: true }] }
+    //        attributes: ['ManagerName']
 
 
- /*   include: [{
+    /*   include: [{
       model: db.ci,
       required: false,
       as: 'ci',
@@ -49,63 +48,69 @@ exports.findAllApp = (req, res) => {
 } */
 
 
-/* {
-   
-  attributes: ['is_valid',['version','versionName'], [Sequelize.col('ci.classService.name'), 'service'] , [Sequelize.col('ci.company'), 'company'] ],
-      include: [{
-          model: db.ci,
-          required: false,
-          as: 'ci',
-          attributes: [  ],
-          include: [{
-            model: db.classService,
-            required: false,
-            as: 'classService',
-            attributes: [],
-            
-        }]
-
-      }],
-      
-  } */
-
-
-
-/*   {
-   
-    attributes: ['is_valid',['version','versionName'], [Sequelize.col('ci.classService.name'), 'service'] , [Sequelize.col('ci.company'), 'company'] ],
-        include: [{ model: db.ci, as: 'ci', attributes: [  ],
-            include: [{ model: db.classService,as: 'classService',attributes: [] }]
-            }],
-        
-  } */
-
-
-//,     [Sequelize.literal('"ci"."company"'), 'companyName']
- app.findAll({
-
-  include: [
-    {  model: db.ci,  required: false, as: 'ci', attributes: [],
-          include: [{   model: db.classService,  required: false,  as: 'classService',  attributes: [], }]
-    },
-    { 
-       model: db.instance, required: false, as: 'instance',  attributes: ['name','isoccurenciable'], 
-    }
-
-],
-attributes: ['version', 'is_valid',[Sequelize.col('ci.classService.name'), 'service'],[Sequelize.col('ci.company'), 'company'] , [Sequelize.col('ci.description'), 'description']]
-
-})
-     .then(data => {
+    /* {
        
-       res.send(data);
-     })
-     .catch(err => {
-       res.status(500).send({
-         message:
-           err.message || "Some error occurred while retrieving Platforms."
-       });
-     });
- };
+      attributes: ['is_valid',['version','versionName'], [Sequelize.col('ci.classService.name'), 'service'] , [Sequelize.col('ci.company'), 'company'] ],
+          include: [{
+              model: db.ci,
+              required: false,
+              as: 'ci',
+              attributes: [  ],
+              include: [{
+                model: db.classService,
+                required: false,
+                as: 'classService',
+                attributes: [],
+                
+            }]
+
+          }],
+          
+      } */
 
 
+
+    /*   {
+       
+        attributes: ['is_valid',['version','versionName'], [Sequelize.col('ci.classService.name'), 'service'] , [Sequelize.col('ci.company'), 'company'] ],
+            include: [{ model: db.ci, as: 'ci', attributes: [  ],
+                include: [{ model: db.classService,as: 'classService',attributes: [] }]
+                }],
+            
+      } */
+
+
+    //,     [Sequelize.literal('"ci"."company"'), 'companyName']
+    app.findAll({
+
+            include: [{
+                    model: db.ci,
+                    required: false,
+                    as: 'ci',
+                    attributes: [],
+                    include: [{ model: db.classService, required: false, as: 'classService', attributes: [], }]
+                },
+                {
+                    model: db.instance,
+                    required: false,
+                    as: 'instance',
+                    attributes: ['name'],
+                }
+
+            ],
+            attributes: ['version', 'is_valid', [Sequelize.col('ci.classService.name'), 'service'],
+                [Sequelize.col('ci.company'), 'company'],
+                [Sequelize.col('ci.description'), 'description']
+            ]
+
+        })
+        .then(data => {
+
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving Platforms."
+            });
+        });
+};
